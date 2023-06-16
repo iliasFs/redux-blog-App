6a567b83cf74dc8e6efabe0b68b73ec3a516a7bc
iliasFs <110726288+iliasFs@users.noreply.github.com>
2023-06-16 17:01:53 +0300

    created store , created slice , and reducers

---
+++ src/app/features/posts/PostsList.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+const PostsList = () => {
+  //const posts = useSelector((state) => state.posts);
+  const posts = useSelector(selectAllPosts)
+  /* -useSelector is used to access the state object in a Redux store
+   -The function being passed to useSelector is a selector function
+   -In this case it takes the entire STATE OBJECT as an argument and returns the posts property from the state
+   -Whenever the posts property changes, the component will be re-rendered to reflect the updated value.*/
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
---
+++ src/app/features/posts/postsSlice.jsx
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+  },
+
+  {
+    id: "2",
+    title: "Slices",
+    content:
+      "In Redux, a slice refers to a portion of the global state managed by the Redux store.",
+  },
+];
+
+const postsSlice = createSlice({
+  //createSlice automatically generates an action creator function with the same name.
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload); //inside of the createSlice we can use.push it doesn't mutate the array.
+    },
+  },
+});
+
+//what if the structure of our state changes in the future?
+//thats why we create a selector in the slice file and export it.
+//so, if the SHAPE of the state changes (not the state because this is covered with the selector in the component. if the state changes it will rerender that component.here we talk about the shape of the state)
+//by creating that we dont need later to change each component individually. we can change it once in the slide.
+
+export const selectAllPosts = (state) => state.posts;
+export const { postAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
